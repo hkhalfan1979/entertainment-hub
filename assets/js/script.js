d6c0f520a0415e5416e05fdeb0773b3621ca56f1
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-04-13 18:49:18 -0400

    tv show API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-var api_key = "491b43ef93087cf91389b9e31b71d2b1"; //TODO insert your unique API KEY here
-var api_url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key;
-var api_url_tv = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key;
+var api_key = "491b43ef93087cf91389b9e31b71d2b1"; //my api key
+var api_url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key; // popular movies API
+var api_url_tv = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key; // popular tv shows API
 
+    // call API and append data to div on index using ".popular-movies" class
 		$.getJSON( api_url, function( data ) {
 
 			$.each( data.results, function( i, item ) {
 				$("<div class='col-md-3'><h3>" + item.title + "</h3></div>").appendTo(".popular-movies");
 			});
 		});
-
+    // call API and append data to div on index using ".popular-shows" class
     $.getJSON( api_url_tv, function( data ) {
 
 			$.each( data.results, function( i, item ) {
