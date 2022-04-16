fb914fe7b9f08a69b0898104c2ad3dfbd11d6877
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-04-16 13:57:01 -0400

    pulled in images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,21 @@ var api_key = "491b43ef93087cf91389b9e31b71d2b1"; //my api key
 var api_url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key; // popular movies API
 var api_url_tv = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key; // popular tv shows API
 
+var search_api_url = "https://api.themoviedb.org/3/search/movie?api_key=491b43ef93087cf91389b9e31b71d2b1&query=star+wars";
+
     // call API and append data to div on index using ".popular-movies" class
 		$.getJSON( api_url, function( data ) {
 
 			$.each( data.results, function( i, item ) {
-				$("<div class='col-md-3'><h3>" + item.title + "</h3></div>").appendTo(".popular-movies");
+				var posterFullUrl = "https://image.tmdb.org/t/p/w185/" + item.poster_path;
+				$("<div class='col-md-3'><img src="+posterFullUrl+"><h3>" + item.title + "</h3></div>").appendTo(".popular-movies");
 			});
 		});
     // call API and append data to div on index using ".popular-shows" class
     $.getJSON( api_url_tv, function( data ) {
 
 			$.each( data.results, function( i, item ) {
-				$("<div class='col-md-3'><h3>" + item.name + "</h3></div>").appendTo(".popular-shows");
+				var posterFullUrl = "https://image.tmdb.org/t/p/w185/" + item.poster_path;
+				$("<div class='col-md-3'><img src="+posterFullUrl+"><h3>" + item.name + "</h3></div>").appendTo(".popular-shows");
 			});
 		});
