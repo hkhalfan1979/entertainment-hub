237571e3a9c0f9c790bcd6e38038fe5fbefe2772
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-04-18 12:20:09 -0400

    append search to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ var api_key = "491b43ef93087cf91389b9e31b71d2b1"; //my api key
 var api_url = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key; // popular movies API
 var api_url_tv = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key; // popular tv shows API
 
-var search_api_url = "https://api.themoviedb.org/3/search/movie?api_key=491b43ef93087cf91389b9e31b71d2b1&query=star+wars";
+
 
     // call API and append data to div on index using ".popular-movies" class
 		$.getJSON( api_url, function( data ) {
 
 			$.each( data.results, function( i, item ) {
 				var posterFullUrl = "https://image.tmdb.org/t/p/w185/" + item.poster_path;
-				$("<div class='col-md-3'><img src="+posterFullUrl+"><h3>" + item.title + "</h3></div>").appendTo(".popular-movies");
+				$("<div class='col-md-3'><img src="+posterFullUrl+"><h3>" + item.title + "</h3><p>"+ item.release_date +"</p></div>").appendTo(".popular-movies");
 			});
 		});
     // call API and append data to div on index using ".popular-shows" class
@@ -44,6 +44,10 @@ var search_api_url = "https://api.themoviedb.org/3/search/movie?api_key=491b43ef
 				   dataType: 'jsonp',
 				   success: function(data) {
 				    console.log(data);
+					$.each( data.results, function( i, item ) {
+						var posterFullUrl = "https://image.tmdb.org/t/p/w185//" + item.poster_path;
+						$("<div class='col-3 mb-1'><img src="+posterFullUrl+"><h3>" + item.title + "</h3></div>").appendTo(".search-movie");
+					});
 				    
 				   },
 				   error: function (request, status, error) {
